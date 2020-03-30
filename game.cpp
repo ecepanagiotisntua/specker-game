aa6672c7f308b380fbb91e571d44b43cabccc4c5
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-30 11:43:43 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,10 +3,10 @@ class Game {
 private:
 int players;
 int heaps;
-static int heap_count;
-static int players_count;
+int heap_count;
+int players_count;
 int *heap_coins;
-Player *player_table;
+Player **player_table;
 
 
 public:
@@ -19,4 +19,35 @@ void play(ostream &out) throw(logic_error);
 };
 
 Game::Game(int heaps, int players){
+  players = players;
+  heaps = heaps;
+  heap_count = 0;
+  player_count = 0;
+  heap_coins = new int[heaps];
+  player_table = new Player*[players];
+}
+
+Game::~Game(){
+  delete [] heap_coins;
+  delete [] player_table;
+}
+
+void Game::addHeap(int coins) throw(logic_error){
+  if (coins < 0) throw logic_error("No negative coins");
+  else if (heap_count >= heaps) throw logic_error("No  more heaps");
+  else{
+    heap_coins[heap_count] = coins;
+    heap_count++;
+  }
+}
+
+void Game::addPlayer(Player *player) throw(logic_error){
+  if (player_count >= players) throw logic_error("No more players");
+  else{
+    player_table[player_count] = player;
+    player_count++;
+  }
+  
+  
+    
 
